57270d7fb8645fd4c6889c4ebde87a4f3a81f62e
Swapnali <swapnalippatil15@gmail.com>
2018-03-17 23:38:21 +0000

    with grid layout

--- app/components/employeeListView.js
+++ app/components/employeeListView.js
@@ -3,13 +3,40 @@ import {render} from 'react-dom';
 
 import employees from '../assets/sample-data.json';
 
+class Employeeitem extends PureComponent {	
+	render(){	
+		let item = this.props.detials;
+		let id = this.props.id;
+		return (			
+		   <div className="item-{id}" key={item.id}>
+		    <a href="" className="card">		      
+		      <img className="thumb"  src={item.avatar} />
+		      <article>
+		      <h1>{item.firstName}.{item.lastName}</h1>
+             <span>{item.jobTitle}</span>
+             </article>
+		      </a>
+           </div>        
+		);
+	}
+}
+
 export default class EmployeeListView extends PureComponent {
 	
 	render(){
+		const employeeview = employees.employees.map(			
+			(item, i) => {					
+				return (<Employeeitem detials={item} id={i} key={i}/>)
+			}
+		);
+		
 		return (
-			<div>
-			    <h1>My React app</h1>
+			<div className="prashant">
+			<div className="band">				
+				{employeeview}
+			</div>
 			</div>
 		);
 	}
 }
+
